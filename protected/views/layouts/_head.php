8780ec8e58457bc66e33675f44c056379206699f
xitrumuit1991 <tranvokhoinguyen@gmail.com>
2015-04-21 09:59:15 +0700

    new

--- protected/views/layouts/_head.php
+++ protected/views/layouts/_head.php
@@ -149,3 +149,17 @@ function xoaTinDetail(object, tin_id, user_id, id_div)
 }(document, 'script', 'facebook-jssdk'));</script>
 <meta property="fb:app_id" content="906605782693204" />
 <meta property="fb:admins" content="100000628072288"/>
+
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60839598-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
